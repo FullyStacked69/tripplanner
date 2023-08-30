70d18fdebe9b3c1c46c7319112bec3117de64517
Elliot Kuok <elliotckuok@gmail.com>
2023-08-29 20:38:42 -0700

    Add map and edit section

--- frontend/src/components/ItineraryEditPage/ItineraryEditPage.js
+++ frontend/src/components/ItineraryEditPage/ItineraryEditPage.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import './ItineraryEditPage.css';
 
-const ItinerariesEditPage = () => {
+const ItineraryEditPage = () => {
     return (
-        <div>
-            <h1>Itinerary Edit Page</h1>
-            {/* Other content related to itineraries */}
+        <div className='page-content-container'>
+            <div id='itinerary-section-container'>
+                <p>Itinerary Edit Section</p>
+
+            </div>
+            <div id='map-section-container'>
+                <p>Map section</p>
+
+            </div>
+            
         </div>
     );
 };
 
-export default ItinerariesEditPage;
+export default ItineraryEditPage;
