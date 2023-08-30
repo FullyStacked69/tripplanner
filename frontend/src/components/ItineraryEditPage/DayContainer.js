040b8c036a05f8fe21191676192fdaf6df9fad8a
Elliot Kuok <elliotckuok@gmail.com>
2023-08-30 14:06:52 -0700

    Add minor styling adjustments

--- frontend/src/components/ItineraryEditPage/DayContainer.js
+++ frontend/src/components/ItineraryEditPage/DayContainer.js
@@ -1,32 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ActivityContainer } from './ActivityContainer';
 
 export function DayContainer({ day, index }) {
-    const radioId = `radio${index}`;
+    const [isOpen, setIsOpen] = useState(false);
 
     return (
         <div className='day-container'>
-            <input type='radio' id={radioId} name='accordion' />
-            <label className='item' htmlFor={radioId}>
+            <div className='item' onClick={() => setIsOpen(!isOpen)}>
                 <div className='title'>
                     <div className='day-detail-container'>
-                        <h3>{day.date}</h3>
-
-                    
+                        <h3>{day.date}</h3>                    
                         <h5>{day.places} places</h5>
                     </div>
+                    <span className='arrow-icon'>{isOpen ? '\u25b2' : '\u25bc'}</span>
                 </div>
-                <div className='content'>
+                <div className='content' style={{ display: isOpen ? 'block' : 'none' }}>
                     <div className='content-container'>
                         <input placeholder='You dont have a place to stay yet!'></input>
                         <ActivityContainer />
                         <input placeholder='Add activities for your trip here'></input>
                         <div>
-
+                            
                         </div>
                     </div>
                 </div>
-            </label>
+            </div>
         </div>
     );
 }
