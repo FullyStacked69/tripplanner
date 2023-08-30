dad003340bbabb5bd931122bd7dc810969d734d7
Wizzerrd <luis@DESKTOP-7AS3T51>
2023-08-30 12:21:15 -0700

    add location search

--- frontend/src/components/MainPage/MainPage.js
+++ frontend/src/components/MainPage/MainPage.js
@@ -10,9 +10,35 @@ function MainPage() {
         startDate: new Date().toDateString(),
         endDate: new Date().toDateString()
     })
+
+    const [foundLocos, setFoundLocos] = useState([])
+    const [searchBarFocus, setSearchBarFocus] = useState(false)
+    const [searching, setSearching] = useState(false)
     
     function handleSearch(){
+        if(searchObj.location){
+            setSearching(true)
+        }
+    }
 
+    const handleInputChange = async e => {
+        await setSearchObj({...searchObj, location: e.target.value})
+        if(e.target.value){
+            const res = await fetch(`/api/locations?location=${e.target.value}`)
+            let data = await res.json()
+            setFoundLocos(data.splice(0, 10))
+        } else {
+            setFoundLocos([])
+        }
+    }
+
+    function LocoOpt(loco){       
+        return(
+            <div onMouseDown={() => setSearchObj({...searchObj, location: loco.name})} className='loco-opt'>
+                <h3>{loco.name}</h3>
+                {(loco.country && <h3>City</h3>) || <h3>Country</h3>}
+            </div>
+        )
     }
     
     return (
@@ -22,13 +48,19 @@ function MainPage() {
                 <h2>Craft, refine, and navigate your adventures using real itineraries from fellow travelers</h2>
             </div>
             <div id="splash-search">
-                <input value={searchObj.location} onChange={e=>setSearchObj({...searchObj, location: e.target.value})} type="text"/>
+                <div id='loco-search-holder'>
+                    <input onFocus={e => setSearchBarFocus(true)} onBlur={e => setSearchBarFocus(false)} value={searchObj.location} onChange={e=> handleInputChange(e)} type="text"/>
+                    <div className='loco-opt-holder'>
+                        {(searchBarFocus && foundLocos) && foundLocos.map(loco => LocoOpt(loco))}
+                    </div>
+                </div>
                 <div id="splash-search-date-range">
                     <input type="date" value={searchObj.startDate} onChange={e=>setSearchObj({...searchObj, startDate: e.target.value})}/>
                     <input type="date" value={searchObj.endDate} onChange={e=> {if(e.target.value > searchObj.startDate)setSearchObj({...searchObj, endDate: e.target.value})}} />
                 </div>
-                <div onClick={()=>handleSearch()} className="myButton small-button">Start Planning</div>
+                <button onClick={()=>handleSearch()} className="myButton small-button">Start Planning</button>
             </div>
+            {searching && <ItineraryList searchObj={searchObj}/>}
         </div>
         
     );
