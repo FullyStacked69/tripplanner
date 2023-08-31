2f6b6f95eb819b43274561a91a68ed75d4e6f1b6
Manuel P <m.perezasensio@gmail.com>
2023-08-31 01:34:55 -0700

    itineraries model validations and seeds updated

--- backend/models/Itinerary.js
+++ backend/models/Itinerary.js
@@ -2,38 +2,38 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const itinerarySchema = new Schema(
-  {
-    title: {
-        type: String,
-        required: false
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        length: {
+            type: Number,
+            required: true,
+        },
+        locationType: {
+            type: String,
+            required: true,
+        },
+        countryCode: {
+            type: String,
+            required: true,
+        },
+        locationName: {
+            type: String,
+            required: true,
+        },
+        lng: {
+            type: Number,
+            required: true,
+        },
+        lat: {
+            type: Number,
+            required: true,
+        },
+        author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     },
-    length: {
-        type: Number,
-        required: true
-    },
-    locationType: {
-        type: String,
-        required: true
-    },
-    countryCode: {
-        type: String,
-        required: true
-    },
-    locationName: {
-        type: String,
-        required: true
-    },
-    lng: {
-        type: Number,
-        required: true
-    },
-    lat: {
-        type: Number,
-        required: true
-    },
-    author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    partOf: {type: Schema.Types.ObjectId, ref: 'Itinerary', required: false},
-    }, { timestamps: true }
+    { timestamps: true }
 );
 
 module.exports = mongoose.model('Itinerary', itinerarySchema);
