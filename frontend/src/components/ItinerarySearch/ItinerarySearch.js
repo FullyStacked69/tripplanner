e69107c10a6ba392cc1762c313c73f7d3c70e88e
Elliot Kuok <elliotckuok@gmail.com>
2023-08-31 16:41:04 -0700

    Added minor styling to location search dropdown

--- frontend/src/components/ItinerarySearch/ItinerarySearch.js
+++ frontend/src/components/ItinerarySearch/ItinerarySearch.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { setSearchObjRedux } from "../../store/searchObj"
 import { LockPlugin, RangePlugin, easepick } from "@easepick/bundle"
-
+import './ItinerarySearch'
 
 export default function ItinerarySearch(){
     const dispatch = useDispatch()
