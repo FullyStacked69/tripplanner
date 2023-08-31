d6f9e98cece6f19853c1f40b3cd1adbe2c8ca3da
DingoDang1113 <Shalpeng109@gmail.com>
2023-08-30 18:09:35 -0700

    merge to itin edit page

--- frontend/src/components/ItineraryEditPage/DayContainer.js
+++ frontend/src/components/ItineraryEditPage/DayContainer.js
@@ -22,12 +22,6 @@ export function DayContainer({ day, index, map, setMarkersPositions, markersPosi
 
     //clear info -> setInfo
 
-    
-
-
-
-
-
     const handleClick = (e) => {
         e.preventDefault();
 
@@ -42,19 +36,15 @@ export function DayContainer({ day, index, map, setMarkersPositions, markersPosi
     return (
         <div className='day-container'>
             <div className='item' onClick={handleClick}>
-                {/* <button type='' value={"collapse"} ></button> */}
-                <div className='title' 
-                     style={{ 
-                         backgroundColor: isOpen ? '#457B9D' : 'transparent', 
-                         color: isOpen ? 'white' : '#457B9D'
-                     }}
-                >
+                <div className='title' style={{ backgroundColor: isOpen ? '#457B9D' : 'transparent', color: isOpen ? 'white' : '#457B9D'}}>
                     <div className='day-detail-container'>
                         <h3>{day.date}</h3>                    
                         <h5>{day.places} places</h5>
                     </div>
+
                     <span className='arrow-icon'>{isOpen ? '\u25b2' : '\u25bc'}</span>
                 </div>
+
                 <div className='content' style={{ display: isOpen ? 'block' : 'none' }}>
                     <div className='content-container'>
                         <input placeholder='You dont have a place to stay yet!'></input>
@@ -64,18 +54,20 @@ export function DayContainer({ day, index, map, setMarkersPositions, markersPosi
 
                         <div>
                             
-                        <div id='recommendations-section'>
-                            <h5>Recommended places close to your hotel or latest activity</h5>
-                            <div id='recommended-activities-container'>
-                                {/* Should iterate through recommended activities and run the info through the RecommendedActivityTile component */}
-                                {sampleActivities.map((sampleActivity, index) => (
-                                    <RecommendedActivityTile  key={index} sampleActivity={sampleActivity} index={index} />
-                                ))}
+                            <div id='recommendations-section'>
+                                <h5>Recommended places close to your hotel or latest activity</h5>
+                                <div id='recommended-activities-container'>
+                                    {/* Should iterate through recommended activities and run the info through the RecommendedActivityTile component */}
+                                    {sampleActivities.map((sampleActivity, index) => (
+                                        <RecommendedActivityTile  key={index} sampleActivity={sampleActivity} index={index} />
+                                    ))}
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    );
+    
+    )
 }
