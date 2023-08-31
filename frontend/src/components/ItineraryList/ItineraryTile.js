589843d553d3b3ee6d3dde67bb29fb825926db8f
Elliot Kuok <elliotckuok@gmail.com>
2023-08-31 12:33:32 -0700

    Add dummy data and basic design of itinerary list

--- frontend/src/components/ItineraryList/ItineraryTile.js
+++ frontend/src/components/ItineraryList/ItineraryTile.js
@@ -1,20 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export function ItineraryTile({itinerary}) {
-    
+    const [isOpen, setIsOpen] = useState(false);
 
 
 
 
     return (
-        <div className='itinerary-container'>
-            <li key={itinerary._id}>
-                <p>{itinerary.title}</p>
-                <p>{itinerary.length}</p>
-                <p>{itinerary.author}</p>
-                <p>{itinerary.likes}</p>
-                <p>{itinerary.views}</p>
-            </li>
+        <div className='day-container'>
+        <div className='item'>
+            <div className='title' onClick={() => setIsOpen(!isOpen)}
+                    style={{ 
+                        backgroundColor: isOpen ? '#457B9D' : 'transparent', 
+                        color: isOpen ? 'white' : '#457B9D'
+                    }}
+                >
+                <div className='intinerary-detail-container'>
+                    <h3>{itinerary.title}</h3>
+                    <div className='itinerary-info'>
+                        <h5>{itinerary.length} days</h5>
+                        <div className='itinerary-details'>
+                            <p>{itinerary.author}</p>
+                            <p>{itinerary.likes}</p>
+                            <p>{itinerary.views}</p>
+                        </div>
+                    </div>          
+                </div>
+
+                <span className='arrow-icon'>{isOpen ? '\u25b2' : '\u25bc'}</span>
+            </div>
+
+            <div className='content' style={{ display: isOpen ? 'block' : 'none' }}>
+                <div className='itinerary-content-container'>                  
+                        {Object.keys(itinerary.days).map(dayNumber => (
+                            <div key={dayNumber}>
+                                <h3>Day {dayNumber}</h3>
+                                <p>{itinerary.days[dayNumber].join(', ')}</p>
+                            </div>
+                        ))}
+                </div>
+            </div>
         </div>
+    </div>
     );
 }
