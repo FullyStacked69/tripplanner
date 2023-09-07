eae2af8f71a9f1fbc2b10d81385a97559da1cf86
Elliot Kuok <elliotckuok@gmail.com>
2023-09-07 00:28:16 -0700

    Add scroll to top functionality

--- frontend/src/components/ItineraryEditPage/ItineraryEditPage.js
+++ frontend/src/components/ItineraryEditPage/ItineraryEditPage.js
@@ -43,7 +43,14 @@ const ItineraryEditPage = () => {
 
     const toggleDropdown = () => {
         setIsDropdownOpen(prev => !prev);
-    };    
+    };
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    }; 
 
     useEffect(() =>{
         if(itineraryId !== 'new'){
@@ -128,8 +135,13 @@ const ItineraryEditPage = () => {
             <div id='itinerary-section-container'>
                 <div id='sidebar-container'>
                     <div id='sidebar'>
-                        <h4>Top Locations</h4>
-                        <h4 onClick={toggleDropdown}>Itinerary</h4>
+                        <div className='sidebar-item'>
+                            <h4 onClick={scrollToTop}>Top Locations</h4>
+                        </div>
+                        <div className='sidebar-item' onClick={toggleDropdown}>
+                            <h4>Itinerary</h4>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16"> <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/> </svg>
+                        </div>
                         {isDropdownOpen && (
                             <ul className="days-dropdown">
                                 {days.map((day, index) => (
