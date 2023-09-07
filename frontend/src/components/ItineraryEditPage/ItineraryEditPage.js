fc635c6be7a8931afe0e9ee2adac5c891c3c7511
DingoDang1113 <Shalpeng109@gmail.com>
2023-09-07 13:10:11 -0700

    revert

--- frontend/src/components/ItineraryEditPage/ItineraryEditPage.js
+++ frontend/src/components/ItineraryEditPage/ItineraryEditPage.js
@@ -6,12 +6,10 @@ import { DayContainer } from './DayContainer';
 import {useJsApiLoader, GoogleMap, Marker, Autocomplete, setZoom, DirectionsRenderer, InfoWindow} from '@react-google-maps/api';
 import MarkerInfoWindow from '../Maps/MarkerInfoWindow';
 import './Maps.css'
-
-
 import { ExploreActivitiesTile } from './ExploreActivitiesTile';
 import './NestedComponents.css'
 import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchItinerary } from '../../store/itineraries';
 
 import northernLightsImg from './assets/northern-lights.jpeg';
@@ -21,17 +19,9 @@ import blueLagoonImg from './assets/blue-lagoon.jpeg';
 import iceClimbingImg from './assets/ice-climbing.avif';
 import fjadrargljufurImg from './assets/fjad.webp'
 
-
 const ItineraryEditPage = () => {
     const dispatch = useDispatch()
-  
-    const dates = [
-            { date: "Saturday, September 9th", places: 5 },
-            { date: "Sunday, September 10th", places: 3 },
-            { date: "Monday, September 11th", places: 2 },
-
-        ];
-
+      
     const pop_activities = [
         {name: "Northern Lights", url: northernLightsImg},
         {name: "Seljalandsfoss", url: seljalandsfossImg},
@@ -41,73 +31,49 @@ const ItineraryEditPage = () => {
         {name: "Fjaðrárgljúfur", url: fjadrargljufurImg},
     ]
     
-    const dateLength = dates.length
-
     const [markersPositions, setMarkersPositions] = useState([]);
-    const [activities, setActivities] = useState([]);
-
-    const initialDays = Array.from({length: dateLength}, (v, idx) => {
-        return {date: `Day ${idx + 1}`, activities: [...activities]}
-    })
-   
-    const [days, setDays] = useState (initialDays)
-    console.log("edit page", days.activities)
-    console.log("edit page", activities)
-
-    const [saveFunc, setSaveFunc] = useState(null)
-
-    const handleSaveButton = () => {
-        if (saveFunc) {
-            saveFunc();
-        }
-    }
-
-      const saveData = () => {
-        return {
-            date: days.date,
-            activities: activities
-        }
-    }
-
-
-
-
-    const sendDataToBackend = async () => {
-        const data = saveData();
-
-        try {
-            const res = await fetch('/its /api/itineraries', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(data)
-            }); 
-
-            if(res.ok) {
-                console.log('Itinerary has been saved');
-            } else {
-                console.error('Cannot save Itinerary', res.statusText);
-            }
-
-        } catch(error) {
-            console.error('Error', error);
-        }
-    };
-    
-    
     
     const [itObj, setItObj] = useState(null)
-    const {itineraryId} = useParams()
-    useEffect(() =>{
-        if(itineraryId){
+    const [days, setDays] = useState([])
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
+    const {searchObj} = useSelector(state => state)
+
+    const {itineraryId} = useParams()
+
+    const toggleDropdown = () => {
+        setIsDropdownOpen(prev => !prev);
+    };
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    };
+
+    const handleDayClick = (index) => {
+        const element = document.getElementById(`day-${index}`);
+        const navbarHeight = 60; 
+        const positionToScrollTo = element.offsetTop - navbarHeight;
+        
+        window.scrollTo({
+            top: positionToScrollTo,
+            behavior: "smooth"
+        });
+    };
+
+    useEffect(() =>{
+        if(itineraryId !== 'new'){
             let it = async () => {
                 const res = await dispatch(fetchItinerary(itineraryId))
                 setItObj(() => res)
+                setDays(() => res.days)
             }
             it()
-            console.log(itObj)
+        } else if (itineraryId === 'new'){
+            let days = []
+            setItObj(()=> ({...searchObj, locationName: searchObj.location, days: days}))
         }
     },[itineraryId])
     
@@ -117,15 +83,10 @@ const ItineraryEditPage = () => {
         lat: splashLat ?? 37.4245,
         lng: splashLng ?? -122.0782
     };
-    // const init = splashPos ?  splashPos : {lat: 37.4245, lng: -122.0782}
-
-    // console.log(itObj)
 
     const [center, setCenter] = useState(splashPos)
 
-
-
-       
+        
     const {isLoaded} = useJsApiLoader({
         googleMapsApiKey: process.env.REACT_APP_MAPS_API_KEY,
         libraries: ['places'],
@@ -182,75 +143,78 @@ const ItineraryEditPage = () => {
     
     return ( 
         <div className='page-content-container'>
-        <div id='itinerary-section-container'>
-            <div id='sidebar'>Sidebar</div>
-            <div id='itinerary-section-content'>
-                <div id='itinerary-tld'>
-                    <div id='title-date-container'>
-                        <h1>{itObj.title}</h1>
-                        <div>9/9 - 9/16</div>
+            <div id='itinerary-section-container'>
+                <div id='sidebar-container'>
+                    <div id='sidebar'>
+                        <div className='sidebar-item'>
+                            <h4 onClick={scrollToTop}>Top Locations</h4>
+                        </div>
+                        <div className='sidebar-item' onClick={toggleDropdown}>
+                            <h4>Itinerary</h4>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16"> <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/> </svg>
+                        </div>
+                        {isDropdownOpen && (
+                            <ul className="days-dropdown">
+                                {days.map((day, index) => (
+                                    <li key={index} onClick={() => handleDayClick(index)}>Day {index + 1}</li>
+                                ))}
+                            </ul>
+                        )}
                     </div>
-                    <div id='itinerary-tld-bttns'>
-                        {/* <button>Share</button> */}
-                        <div></div>
-                        <div id='members-container'>
-                            <div id='member-icon'>E</div>
-                            <div>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-plus-fill" viewBox="0 0 16 16"> <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/> <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/> </svg>
+                </div>
+                <div id='itinerary-section-content'>
+                    <div id='itinerary-tld'>
+                        <div id='title-date-container'>
+                            <h1>{itObj.title}</h1>
+                            <div>9/9 - 9/16</div>
+                        </div>
+                        <div id='itinerary-tld-bttns'>
+                            {/* <button>Share</button> */}
+                            <div></div>
+                            <div id='members-container'>
+                                <div id='member-icon'>E</div>
+                                <div>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-plus-fill" viewBox="0 0 16 16"> <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/> <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/> </svg>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div id='popular-activities-section'>
-                    <div id='activity-list'>
-                        <h2>Top locations for {itObj.locationName}</h2> 
-                        
-                        <div id='popular-activities-container'>
-                            {pop_activities.map((activity, idx) => (
-                                <ExploreActivitiesTile key={idx} activity={activity} />
-                            ))}
+                    <div id='popular-activities-section'>
+                        <div id='activity-list'>
+                            <h2>Top locations for {itObj.locationName}</h2> 
+                            
+                            <div id='popular-activities-container'>
+                                {pop_activities.map((activity, idx) => (
+                                    <ExploreActivitiesTile key={idx} activity={activity} />
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                    <div id='itinerary-container'>
+                        <div id='itinerary-header'>
+                            <h2>Itinerary</h2>   
+                            <a>Collapse All</a>
+                        </div>
+                        <div id='itineary-days-container'>
+                            {itObj && itObj.days.map((day, index) => (
+                                <DayContainer id={`day-${index}`} key={index} day={day} index={index} map={map} setMarkersPositions={setMarkersPositions} markersPositions={markersPositions} setCenter={setCenter} />
+                                ))}
                         </div>
                     </div>
                 </div>
-                <div id='itinerary-container'>
-                    <div id='itinerary-header'>
-                        <h2>Itinerary</h2>
-
-                        {/* <Search map={map} setMarkersPositions={setMarkersPositions} markersPositions={markersPositions} setCenter={setCenter}/> */}
-                        
-                        <a>Collapse All</a>
-                        {/* <a>Collapse All</a> */}
-                    </div>
-                    <div id='itineary-days-container'>
-                        {days.map((day, index) => (
-                            <DayContainer key={index} day={day} index={index} map={map} setMarkersPositions={setMarkersPositions} markersPositions={markersPositions} setCenter={setCenter} activities={activities} setActivities = {setActivities} onSave={setSaveFunc} />
-                            ))}
-                        <button onClick={handleSaveButton}>Save</button>
-                    </div>
-                </div>
             </div>
-
-        </div>
         
-            {/* <Search map={map} setMarkersPositions={setMarkersPositions} markersPositions={markersPositions} setCenter={setCenter}/> */}
-            {/* <Search map={map} setMarkersPositions={setMarkersPositions} markersPositions={markersPositions}/> */}
             <div id='sticky'>
-
             <GoogleMap 
             onLoad={onLoad}
-            // ref={mapRef}
             center={center}
-            // zoom={7}
             mapContainerClassName="map-container"
             >
-                {/* <Marker position={center} /> */}
-                {/* <Marker position={{lat:37.96, lng:-122.0296}} /> */}
                 {markersPositions.map((place, idx) => <MarkerInfoWindow key={idx} place={place} position={{ lat: place.geometry.location.lat(), lng: place.geometry.location.lng() }} />)}
-                {/* <InfoWindow /> */}
             </GoogleMap>
             </div>
-    </div>
+        </div>
     )
 }    
 
-    export default ItineraryEditPage;
+export default ItineraryEditPage;
