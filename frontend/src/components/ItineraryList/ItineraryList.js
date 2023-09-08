d92c76a6ae0be664b5d4e15cf88cfaabd147fb87
Elliot Kuok <elliotckuok@gmail.com>
2023-09-07 18:36:34 -0700

    Add sorting functionality

--- frontend/src/components/ItineraryList/ItineraryList.js
+++ frontend/src/components/ItineraryList/ItineraryList.js
@@ -11,19 +11,30 @@ export default function ItineraryList({ searchObj }) {
     const { startDate, endDate, location } = searchObj;
     const dispatch = useDispatch();
     const itineraries = useSelector(state => state.itineraries);
+    const [sortedItineraries, setSortedItineraries] = useState([])
 
     const handleSort = (criteria) => {
-        // When seed data gets added, sort itineraries based on sortCriteria here.
-        // Simple example sorting by 'likes':
         if (criteria === "likes") {
-            itineraries.sort((a, b) => b.likes - a.likes);
+            const sorted = [...itineraries].sort((a, b) => b.fakeLikes - a.fakeLikes);
+            setSortedItineraries(sorted);
         }
-    };
+        if (criteria === "author") {
+            const sorted = [...itineraries].sort((a, b) => a.author.username.localeCompare(b.author.username));
+            setSortedItineraries(sorted);
+        }
+        if (criteria === "tripLength") {
+            const sorted = [...itineraries].sort((a, b) => b.length - a.length);
+            setSortedItineraries(sorted);
+        }
+    };     
     
     useEffect(() => {
         dispatch(fetchItineraries(location));
     }, [dispatch, location]);
-
+    
+    useEffect(() => {
+        setSortedItineraries(itineraries);
+    }, [itineraries]);    
 
     return(
         <div id='list-page-content-container'>
@@ -44,7 +55,7 @@ export default function ItineraryList({ searchObj }) {
                             </select>
                         </div>
                         <ul>
-                            {itineraries.map(itinerary => (
+                            {sortedItineraries.map(itinerary => (
                                 <ItineraryTile key={itinerary._id} itinerary={itinerary} />
                             ))}
                         </ul>
@@ -57,93 +68,3 @@ export default function ItineraryList({ searchObj }) {
     )
   }
 
-
-
-  // const itineraries = [
-  //     {
-  //       "title": "Adventure seekers Iceland Roadtrip",
-  //       "length": 7,
-  //       "author": "Bilbo Baggins",
-  //       "likes": 21,
-  //       "views": 340,
-  //       "days": {
-  //         "1": ["Hiking to Skogafoss", "Visit to Seljalandsfoss"],
-  //         "2": ["Exploring the Black Sand Beaches"],
-  //         "3": ["Golden Circle Route: Geysir, Gullfoss", "Thingvellir National Park"],
-  //         "4": ["Relaxing at the Blue Lagoon"],
-  //         "5": ["Skaftafell National Park Adventure"],
-  //         "6": ["Jokulsarlon Glacier Lagoon"],
-  //         "7": ["Return to Reykjavik"]
-  //       }
-  //     },
-  //     {
-  //       "title": "Exploring the Land of Fire and Ice",
-  //       "length": 10,
-  //       "author": "Eowyn Snow",
-  //       "likes": 45,
-  //       "views": 890,
-  //       "days": {
-  //         "1": ["Arrival in Reykjavik", "Exploring the City"],
-  //         "2": ["Silfra Fissure Snorkeling"],
-  //         "3": ["South Coast Discovery"],
-  //         "4": ["Vatnajokull Glacier Hike"],
-  //         "5": ["Akureyri and Northern Beauty"],
-  //         "6": ["Whale Watching"],
-  //         "7": ["East Fjords Exploration"],
-  //         "8": ["Myvatn Nature Baths"],
-  //         "9": ["Húsavík: The Whale Watching Capital"],
-  //         "10": ["Return to Reykjavik"]
-  //       }
-  //     },
-  //     {
-  //       "title": "Icelandic Wonders: From Reykjavik to the Glaciers",
-  //       "length": 8,
-  //       "author": "Aragorn Frost",
-  //       "likes": 72,
-  //       "views": 1250,
-  //       "days": {
-  //         "1": ["Arrival in Reykjavik"],
-  //         "2": ["Reykjavik City Tour"],
-  //         "3": ["Golden Circle Exploration"],
-  //         "4": ["Into the Glacier: Langjokull Ice Tunnel"],
-  //         "5": ["Borgarfjordur and Hraunfossar"],
-  //         "6": ["Exploring Westfjords"],
-  //         "7": ["Dynjandi Waterfall Hike"],
-  //         "8": ["Return to Reykjavik"]
-  //       }
-  //     },
-  //     {
-  //       "title": "Northern Lights Quest: Chasing Aurora Borealis in Iceland",
-  //       "length": 5,
-  //       "author": "Gimli Aurora",
-  //       "likes": 30,
-  //       "views": 620,
-  //       "days": {
-  //         "1": ["Arrival in Reykjavik", "Evening Northern Lights Hunt"],
-  //         "2": ["Golden Circle and Geothermal Pools"],
-  //         "3": ["South Coast Adventure"],
-  //         "4": ["Glacier Hike and Ice Cave Exploration"],
-  //         "5": ["Return to Reykjavik"]
-  //       }
-  //     },
-  //     {
-  //       "title": "Ring Road Adventure: Circling the Beauty of Iceland",
-  //       "length": 14,
-  //       "author": "Legolas Rivers",
-  //       "likes": 56,
-  //       "views": 1100,
-  //       "days": {
-  //         "1": ["Start from Reykjavik", "Exploring the Snæfellsnes Peninsula"],
-  //         "2": ["Northwest Fjords Discovery"],
-  //         "3": ["Akureyri and Surroundings"],
-  //         "4": ["Myvatn and Surrounding Attractions"],
-  //         "5": ["Eastern Fjords and Seydisfjordur"],
-  //         "6": ["Hofn and Southeast Adventures"],
-  //         "7": ["Vatnajokull National Park"],
-  //         "8": ["Jokulsarlon Glacier Lagoon"],
-  //         "9": ["South Coast Highlights"],
-  //         "10": ["Golden Circle Attractions"],
-  //         "11": ["Return to Reykjavik"]
-  //       }
-  //     }
-  //   ]
