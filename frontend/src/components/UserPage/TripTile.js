779af121ba27079e9d6ce120185573403f4b6009
Elliot Kuok <elliotckuok@gmail.com>
2023-09-11 22:09:48 -0700

    Fix date format on trip tile

--- frontend/src/components/UserPage/TripTile.js
+++ frontend/src/components/UserPage/TripTile.js
@@ -1,8 +1,20 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 
 export default function TripTile({trip}) {
 
+    const { startDate, length } = trip;
+    const start = new Date(startDate);
+
+    const end = new Date(startDate);
+    end.setDate(end.getDate() + length - 1);
+
+    const formattedStartDate = formatDate(start);
+    const formattedEndDate = formatDate(end);
+
+    function formatDate(dateObj) {
+        const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+        return `${dateObj.getDate()} ${months[dateObj.getMonth()]} ${dateObj.getFullYear()}`;
+    }
 
     return (
         <div className='trip-container'>
@@ -11,7 +23,7 @@ export default function TripTile({trip}) {
             </div>
             <div className='trip-details'>
                 <h4>{trip.title}</h4>
-                <p>{trip.date}</p>
+                <p>{`${formattedStartDate} - ${formattedEndDate}`}</p>
             </div>
         </div>
     );
