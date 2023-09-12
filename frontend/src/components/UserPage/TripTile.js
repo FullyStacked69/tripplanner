63fb97624ff199557cc4e6415150b6dc44c763df
Elliot Kuok <elliotckuok@gmail.com>
2023-09-11 22:27:59 -0700

    Add link functionality on tile to corresponding itinerary page

--- frontend/src/components/UserPage/TripTile.js
+++ frontend/src/components/UserPage/TripTile.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function TripTile({trip}) {
 
-    const { startDate, length } = trip;
+    const { startDate, length, _id } = trip;
     const start = new Date(startDate);
 
     const end = new Date(startDate);
@@ -17,14 +18,16 @@ export default function TripTile({trip}) {
     }
 
     return (
-        <div className='trip-container'>
-            <div className='trip-img-container'>
-                <img></img>
+        <Link to={`/itineraries/${_id}/plan`}>
+            <div className='trip-container'>
+                <div className='trip-img-container'>
+                    <img></img>
+                </div>
+                <div className='trip-details'>
+                    <h4>{trip.title}</h4>
+                    <p>{`${formattedStartDate} - ${formattedEndDate}`}</p>
+                </div>
             </div>
-            <div className='trip-details'>
-                <h4>{trip.title}</h4>
-                <p>{`${formattedStartDate} - ${formattedEndDate}`}</p>
-            </div>
-        </div>
+        </Link>
     );
 }
