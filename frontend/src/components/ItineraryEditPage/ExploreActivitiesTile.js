a41a31c82c9a8737473b28f7d6821105adc62122
Wizzerrd <luisthelaffitte@gmail.com>
2023-09-13 13:14:47 -0700

    Merge branch 'maps' into it-edit

--- frontend/src/components/ItineraryEditPage/ExploreActivitiesTile.js
+++ frontend/src/components/ItineraryEditPage/ExploreActivitiesTile.js
@@ -5,7 +5,7 @@ export function ExploreActivitiesTile({activity}) {
 
     return (
         <div className='popular-activity-container'>
-            <img src={activity.url}/>
+            <img src={activity.url} style={{ width: '100%', height: '150px', borderRadius: '10px'}} />
             <div className='popular-activity-name'>{activity.name}</div>
         </div>
     );
