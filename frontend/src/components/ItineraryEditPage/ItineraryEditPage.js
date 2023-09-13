cc20a5db1e7ba1ca948d0c951c0d3bd1ccd24b42
DingoDang1113 <Shalpeng109@gmail.com>
2023-09-13 13:40:22 -0700

    push to main

--- frontend/src/components/ItineraryEditPage/ItineraryEditPage.js
+++ frontend/src/components/ItineraryEditPage/ItineraryEditPage.js
@@ -20,7 +20,8 @@ const ItineraryEditPage = () => {
     const dispatch = useDispatch()
     
     const [markersPositions, setMarkersPositions] = useState([]);
-    
+    const [redirectTo, setRedirectTo] = useState('');
+
     const {itObj} = useSelector(state => state.itineraries)
     const { user } = useSelector(state => state.session)
     const [days, setDays] = useState([])
@@ -233,6 +234,7 @@ const ItineraryEditPage = () => {
                 console.log(newItiniterary)
                 await dispatch(createItinerary(newItiniterary));
                 console.log("Itinerary has been saved")
+                // setRedirectTo(``)
             } catch (error) {
                 console.error("Error saving itinerary:", error);
             }
@@ -276,7 +278,7 @@ const ItineraryEditPage = () => {
                         </div>
                         {isDropdownOpen && (
                             <ul className="days-dropdown">
-                                {days.map((day, index) => (
+                                {itObj.days.map((day, index) => (
                                     <li key={index} onClick={() => handleDayClick(index)}>Day {index + 1}</li>
                                 ))}
                             </ul>
@@ -295,9 +297,9 @@ const ItineraryEditPage = () => {
                             {/* <button>Share</button> */}
                             <div></div>
                             <div id='members-container'>
-                                <div id='member-icon'>E</div>
+                                {/* <div id='member-icon'>E</div> */}
                                 <div>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-plus-fill" viewBox="0 0 16 16"> <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/> <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/> </svg>
+                                    {/* <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-plus-fill" viewBox="0 0 16 16"> <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/> <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/> </svg> */}
                                 </div>
                             </div>
                         </div>
