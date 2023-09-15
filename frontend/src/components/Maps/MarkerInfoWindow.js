2bb1b653ee26419f56173bcf463bbdc5f4e4f964
DingoDang1113 <Shalpeng109@gmail.com>
2023-09-14 20:27:29 -0700

    clean up file

--- frontend/src/components/Maps/MarkerInfoWindow.js
+++ frontend/src/components/Maps/MarkerInfoWindow.js
@@ -6,27 +6,7 @@ const MarkerInfoWindow = ({position, place}) => {
 
     const [isOpen, setIsOpen] = useState(false);
 
-    return (
-        // <>
-
-        //     <Marker  position={position}  onClick={() => setIsOpen(true)}/>
-        //         { isOpen &&  <InfoWindow  position={{lat:position.lat, lng:position.lng }} onCloseClick={() => setIsOpen(false)}>
-        //             <div>
-        //                 <h3>{place?.name}</h3>
-        //                 {/* <p>{place.formatted_address}</p> */}
-        //                 <p>{place?.address}</p>
-        //                 <p>{place?.phone}</p>
-        //                 {/* <p>{place.current_opening_hours.weekday_text}</p> */}
-        //                 {place?.rating && <p>Rating: {place?.rating}({place?.user_ratings_total})</p>}
-        //                 {/* <p>Review: {place.reviews[0].text}</p> */}
-        //                 {/* <img src={place.photos[0].getUrl()}   /> */}
-
-        //                 {/* <p> name, address, rating, opening</p> */}
-        //             </div>
-
-        //         </InfoWindow>}
-        // </>
-        
+    return (    
         <>
             <Marker 
                 // key={idx} 
